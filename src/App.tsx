33a25cf1d37f897965d215ffc38299c66ff0b265
Agustina Carrion <agus.carrion@gmail.com>
2023-04-19 13:07:12 +0200

    chore: add custom colors and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+
+
 const App = () => {
-  return <div className="bg-black h-full w-full"></div>;
+  return (
+    <div className="bg-satin-linen h-full w-full">
+      <h1 className="font-bold text-4xl p-5">Gabriela Cresta</h1>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "@fontsource/montserrat";
+import "@fontsource/montserrat/700.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
