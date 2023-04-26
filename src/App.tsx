64dd059a06a4d253485e2b4118bba82aa09525ac
Moritz Stückler <moritz@bitbetter.de>
2023-04-26 10:43:35 +0200

    feat: image scaling

--- src/App.tsx
+++ src/App.tsx
@@ -12,23 +12,31 @@ const App = () => {
   return (
     <div className="bg-satin-linen h-full w-full">
       <Header />
-      <div className="flex justify-between mb-40">
-        <div className="w-1/2 -mx-20 md:-mx-40 xl:-mx-64 mt-8">
-          <img
-            src="/src/assets/images/TocadoPrueba.png"
-            alt="Tocado de flores secas"
-            className="rotate-[130deg] scale-125 md:scale-110 pointer-events-none"
-          />
-        </div>
-        <div className="w-1/2 -mx-20 md:-mx-40 xl:-mx-64 mt-8">
-          <img
-            src="/src/assets/images/TocadoPrueba.png"
-            alt="Tocado de flores secas"
-            className="-rotate-[65deg] scale-125 md:scale-110 pointer-events-none"
-          />
-        </div>
+      <div className="relative mb-40">
+        <SideImage className="inline-block left-0" imgClassName="rotate-[130deg] -translate-x-[40%]" />
+        <SideImage className="inline-block right-0" imgClassName="-rotate-[65deg] translate-x-[40%]" />
       </div>
       <Carousel items={items} />
+    </div>
+  );
+};
+
+const SideImage = ({
+  className,
+  imgClassName,
+}: {
+  className?: string;
+  imgClassName?: string;
+}) => {
+  return (
+    <div className={`min-h-[160px] h-[20vw] absolute z-10 ${className || ""}`}>
+      <img
+        src="images/TocadoPrueba.png"
+        alt="Tocado de flores secas"
+        className={`object-contain pointer-events-none h-full ${
+          imgClassName || ""
+        }`}
+      />
     </div>
   );
 };
